6dda383a7a52370f32a23f63aed7bbdb8adf3cb0
huyền Trịnh <00thanhthanhhuyen@gmail.com>
2024-11-02 21:43:57 +0700

    update

--- app/routes/main.py
+++ app/routes/main.py
@@ -8,7 +8,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
 
-    return render_template('index.html')
+    return render_template('report.html')
 
 @main.route('/home')
 def home():
