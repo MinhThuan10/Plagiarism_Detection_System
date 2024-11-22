11b94e2b11016cd507962b2d64e992ac5a3f042a
Minh Thuận <147970223+MinhThuan10@users.noreply.github.com>
2024-11-22 21:56:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     SECRET_KEY = 'thuan'
-    MONGO_URI = 'mongodb+srv://minhthuan:minhthuan@minhthuan.vkhimqg.mongodb.net/mydatabase'
-    # MONGO_URI = 'mongodb://localhost:27017/mydatabase'
+    # MONGO_URI = 'mongodb+srv://minhthuan:minhthuan@minhthuan.vkhimqg.mongodb.net/mydatabase'
+    MONGO_URI = 'mongodb://localhost:27017/mydatabase'
     ELASTICSEARCH_URL = 'http://localhost:9200'
