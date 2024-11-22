844af6c8b22a5dda8e6cdc28675ac321605d52d5
Minh Thuận <147970223+MinhThuan10@users.noreply.github.com>
2024-11-22 21:23:18 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 # run.py
-# waitress-serve --listen=127.0.0.1:8000 run:app
 from app import create_app
 from waitress import serve
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+
+    port = int(os.getenv('PORT', 5000))  # Cổng Render cung cấp
+    app.run(debug=False, host='0.0.0.0', port=port)
