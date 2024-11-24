2b47dd682fe3eb42b69da4dba0191ed4acedb512
Minh Thuận <147970223+MinhThuan10@users.noreply.github.com>
2024-11-24 16:23:21 +0700

    update

--- app/models/search_system/search_google.py
+++ app/models/search_system/search_google.py
@@ -6,72 +6,7 @@ from docx import Document
 from io import StringIO
 import pandas as pd
 from bs4 import BeautifulSoup
-
-API_KEYS = [
-    'AIzaSyAuWuy0Up52QUeKU-iYb7pNtZgu09rHk3g',
-    'AIzaSyAQfDwx60quGbjqLeKOwk4DTl27s180cDE',
-    'AIzaSyBf9wrYHM4SYW9w-jvA-PIIF-VJKI4owaA',
-    'AIzaSyBU5mrYJLK3cGAuG3hlSDHsDFgZoWDauQM',
-    'AIzaSyAn3PKdLNNuy6dc2zUcPnRiXuiSYFmLrpg',
-    'AIzaSyATVTCw6KpsRrTpot-B8M5ON0J06uULkD4',
-    'AIzaSyBpjtdhFaONvHjaf-YLZqSBMwigv5OetnA',
-    'AIzaSyBLwEhO1xq-p57LR5NjccC0ym2_ZdV-oSI',
-    'AIzaSyBwIrGwgFYQI47PrY3VTnvrEuUC4Ei5V_Q',
-    'AIzaSyB4dVUMkfQE5fd5-ehopPC6XM3pl0oQerk',
-    'AIzaSyAF73pow1YLtPLsyxT7mnGd0eB4nMdzRJU',
-
-]
-CX = 'f5a8b4e14ca474f61'
-
-API_KEYS2 = [
-    'AIzaSyDBhQBb0pKBPTh1iiLikdtecc7PW9XaGFY',
-    'AIzaSyBjbKPbW6nLb6lj9_a1inSitoSKkH0wo7o',
-    'AIzaSyC35aTxM9bZpW4XFLiN0cZvMgLG83eKJRs',
-    'AIzaSyAokaQBIB3uaUxAm9mZjpktV5YzaP8gYLI',
-    'AIzaSyAh4EtsRgwRZLdgN3W2mTqfPj6AV2qdWwI',
-    'AIzaSyBXv3I1N6DqClEJwZqZJpNT15272slb5uw',
-    'AIzaSyCTcoSHq5-y1PYhePN9plTp3p27b1NKVWA',
-    'AIzaSyD5lIcE4onfdWFcigcGfXMAP6UM0Zuk2Uk',
-    'AIzaSyDAcREUVSuy4sfKs2Lh4OxuBvgF_rV9SqI',
-    'AIzaSyBHOKyYLiSWETnNH0-e8ikQWmeN0BKi7BU',
-    'AIzaSyBWx3xGBXLHO-XWys5LxIlOHodjDrU6lGA',
-    'AIzaSyBt43IZ77nq8BwvLpaMDUTEFAfBGdlPqoo',
-    
-]
-
-CX2 = 'a6694d6bf858e42f6'
-
-API_KEYS3 = [
-    'AIzaSyB9pd6wJ5oEO4-AN5F0kjtMKXH69ky3oe8',
-    'AIzaSyBhEFWEralUvFNsuqjrwZhMX__YjbBIDeo',
-    'AIzaSyC5965PVE6g5mhSL_nSZoDcxFihduk3Vok',
-    'AIzaSyCKAEdJwGwhiJxA8bGXm_lbi3ZHnSRGCGc',
-    'AIzaSyDEKcMT2Sn0xNJGLFhXt6X4d9t0D5onw5A',
-    'AIzaSyD_z3VYwanzMQcrCiRan-5jplIhAJKLnoM',
-    'AIzaSyB4odGdiBhlrZcrOrngwcHIM_FlLweCX80',
-    'AIzaSyBPRxRJWlr8q4v81xGMshDyJKpc_dGWODg',
-    'AIzaSyALCy-7PoY_SZ425b3V4IBMnTLcrm2QFmg',
-    'AIzaSyDQq2ppxwmYWtW8aGQUi9RTh1g7tpk6KOc',
-    'AIzaSyBmBPKHZAXMd35HjNDCEMMeSpqgp4CDcBo',
-    'AIzaSyB-dx-CFUpzfsGagGT9-4wiYAhc4PsIyD0',
-]
-CX3 = 'c0c0ab4e2da9b4ef2'
-
-API_KEYS4 = [
-    'AIzaSyB3xLylKi869uTqRwOeZBMXPaHFOE4_WG0',
-    'AIzaSyApQdG_sSdiG6zkUDzagXipLQFKM2O8mww',
-    'AIzaSyDbSYjRWLeAHgDXohIORPxL4TG3wEMEnt0',
-    'AIzaSyDFQcrxtpCvA3u98RuqYsdNhqOehX8VIe4',
-    'AIzaSyAwECCGT_p6wOWz3cIUNJptiN0dnPRv8hs',
-    'AIzaSyBwwA43nAbL_rNJrVs0Tx85elMrOg3Sm-U',
-    'AIzaSyBqHNzB_YT8GwQML1x-eAcDXh3Kl9y1FDo',
-    'AIzaSyArm92_Il9cFufWYN6zHnFwgjPodhvALHY',
-    'AIzaSyCiRnAnm-RvoczOfcJ_Jp_uYEHjP8X5irQ',
-    'AIzaSyBAoedWrVtHO-0iVuZKqzUwwpzMDlppqhE',
-    'AIzaSyD0jh0n0mEsd_acUt7yTuLasXeHQk6auGU',
-    'AIzaSyAe4kQFGeVqXnD0-EIx5qAUxA4aeauTx_Y',
-]
-CX4 = '01d819c8b90df4a04'
+from config import Config
 
 
 # Khởi tạo API Key hiện tại
@@ -82,14 +17,14 @@ current_list = 1
 def get_current_api_key():
     """Trả về API Key hiện tại."""
     global current_list
-    if current_list == 1 and current_api_key_index < len(API_KEYS):
-        return API_KEYS[current_api_key_index]
-    elif current_list == 2 and current_api_key_index < len(API_KEYS2):
-        return API_KEYS2[current_api_key_index]
-    elif current_list == 3 and current_api_key_index < len(API_KEYS3):
-        return API_KEYS3[current_api_key_index]
-    elif current_list == 4 and current_api_key_index < len(API_KEYS4):
-        return API_KEYS4[current_api_key_index]
+    if current_list == 1 and current_api_key_index < len(Config.API_KEYS):
+        return Config.API_KEYS[current_api_key_index]
+    elif current_list == 2 and current_api_key_index < len(Config.API_KEYS2):
+        return Config.API_KEYS2[current_api_key_index]
+    elif current_list == 3 and current_api_key_index < len(Config.API_KEYS3):
+        return Config.API_KEYS3[current_api_key_index]
+    elif current_list == 4 and current_api_key_index < len(Config.API_KEYS4):
+        return Config.API_KEYS4[current_api_key_index]
     return None
 
 def get_next_api_key():
@@ -98,19 +33,19 @@ def get_next_api_key():
     current_api_key_index += 1
 
     # Kiểm tra nếu danh sách hiện tại hết API Key
-    if current_list == 1 and current_api_key_index >= len(API_KEYS):
+    if current_list == 1 and current_api_key_index >= len(Config.API_KEYS):
         # Chuyển sang danh sách API_KEYS2
         current_list = 2
         current_api_key_index = 0  # Reset lại index cho API_KEYS2
-    elif current_list == 2 and current_api_key_index >= len(API_KEYS2):
+    elif current_list == 2 and current_api_key_index >= len(Config.API_KEYS2):
         # Chuyển sang danh sách API_KEYS2
         current_list = 3
         current_api_key_index = 0  # Reset lại index cho API_KEYS2
-    elif current_list == 3 and current_api_key_index >= len(API_KEYS3):
+    elif current_list == 3 and current_api_key_index >= len(Config.API_KEYS3):
         # Chuyển sang danh sách API_KEYS2
         current_list = 4
         current_api_key_index = 0  # Reset lại index cho API_KEYS2
-    elif current_list == 4 and current_api_key_index >= len(API_KEYS4):
+    elif current_list == 4 and current_api_key_index >= len(Config.API_KEYS4):
         return None  # Đã dùng hết cả hai danh sách
 
     return get_current_api_key()
@@ -129,11 +64,13 @@ def search_google(query):
         
         # Chọn CX tương ứng với danh sách API
         if current_list == 1:
-            cx = CX
+            cx = Config.CX
         elif current_list == 2:
-            cx = CX2
+            cx = Config.CX2
+        elif current_list == 3:
+            cx = Config.CX3
         else:
-            cx = CX3
+            cx = Config.CX4
         url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={api_key}&cx={cx}"
         response = requests.get(url, verify=False)
         
