d2ae04e48822ac408d3404c424cd2352ee971a3a
Minh Thuận <147970223+MinhThuan10@users.noreply.github.com>
2024-11-24 09:55:15 +0700

    update

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 class Config:
-    SECRET_KEY = 'thuan'
-    # MONGO_URI = 'mongodb+srv://minhthuan:minhthuan@minhthuan.vkhimqg.mongodb.net/mydatabase'
-    MONGO_URI = 'mongodb://localhost:27017/mydatabase'
-    ELASTICSEARCH_URL = 'http://localhost:9200'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    MONGO_URI = os.getenv('MONGO_URI')
+    ELASTICSEARCH_URL = os.getenv('ELASTICSEARCH_URL')
+    CX = os.getenv('CX')
+    CX2 = os.getenv('CX2')
+    CX3 = os.getenv('CX3')
+    CX4 = os.getenv('CX4')
+    API_KEYS = os.getenv('API_KEYS')
+    API_KEYS2 = os.getenv('API_KEYS2')
+    API_KEYS3 = os.getenv('API_KEYS3')
+    API_KEYS4 = os.getenv('API_KEYS4')
