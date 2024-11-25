2e03bb0b45099e8e67eb34a7894cd5628d70ce5d
Minh Thuận <147970223+MinhThuan10@users.noreply.github.com>
2024-11-25 16:02:09 +0700

    update

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 # run.py
 from app import create_app
+import os
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
 
-    # port = int(os.getenv('PORT', 5000))  # Cổng Render cung cấp
-    # app.run(debug=False, host='0.0.0.0', port=port)
+    port = int(os.getenv('PORT', 5000))  # Cổng Render cung cấp
+    app.run(debug=False, host='0.0.0.0', port=port)
